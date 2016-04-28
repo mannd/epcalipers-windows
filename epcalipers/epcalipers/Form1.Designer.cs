64e2bc9ef03db6a97a2a0cd6db672c4774fec1ed
mannd <mannd@DESKTOP-KS9168F>
2016-04-28 10:44:56 -0600

    PictureBox scrolls and "zooms"
    Zoom implemented crudely and brokenly.  Need to change scale so that
    midpoint of slider is 1, with lesser zoom in and zoom out increments.

--- epcalipers/epcalipers/Form1.Designer.cs
+++ epcalipers/epcalipers/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.addCaliper = new System.Windows.Forms.Button();
             this.calibrateButton = new System.Windows.Forms.Button();
@@ -37,19 +36,25 @@
             this.meanRRButton = new System.Windows.Forms.Button();
             this.qtcButton = new System.Windows.Forms.Button();
             this.imageButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 0F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 83.33334F));
             this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.trackBar1, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -59,19 +64,9 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(688, 482);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(682, 427);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
             // flowLayoutPanel1
             // 
+            this.flowLayoutPanel1.AutoSize = true;
             this.flowLayoutPanel1.Controls.Add(this.addCaliper);
             this.flowLayoutPanel1.Controls.Add(this.calibrateButton);
             this.flowLayoutPanel1.Controls.Add(this.intervalRateButton);
@@ -79,14 +74,15 @@
             this.flowLayoutPanel1.Controls.Add(this.qtcButton);
             this.flowLayoutPanel1.Controls.Add(this.imageButton);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 436);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(117, 436);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(682, 43);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(568, 43);
             this.flowLayoutPanel1.TabIndex = 2;
             // 
             // addCaliper
             // 
-            this.addCaliper.Location = new System.Drawing.Point(3, 3);
+            this.addCaliper.Location = new System.Drawing.Point(490, 3);
             this.addCaliper.Name = "addCaliper";
             this.addCaliper.Size = new System.Drawing.Size(75, 23);
             this.addCaliper.TabIndex = 6;
@@ -96,7 +92,7 @@
             // calibrateButton
             // 
             this.calibrateButton.AutoSize = true;
-            this.calibrateButton.Location = new System.Drawing.Point(84, 3);
+            this.calibrateButton.Location = new System.Drawing.Point(409, 3);
             this.calibrateButton.Name = "calibrateButton";
             this.calibrateButton.Size = new System.Drawing.Size(75, 23);
             this.calibrateButton.TabIndex = 1;
@@ -107,7 +103,7 @@
             // intervalRateButton
             // 
             this.intervalRateButton.AutoSize = true;
-            this.intervalRateButton.Location = new System.Drawing.Point(165, 3);
+            this.intervalRateButton.Location = new System.Drawing.Point(323, 3);
             this.intervalRateButton.Name = "intervalRateButton";
             this.intervalRateButton.Size = new System.Drawing.Size(80, 23);
             this.intervalRateButton.TabIndex = 2;
@@ -117,7 +113,7 @@
             // meanRRButton
             // 
             this.meanRRButton.AutoSize = true;
-            this.meanRRButton.Location = new System.Drawing.Point(251, 3);
+            this.meanRRButton.Location = new System.Drawing.Point(242, 3);
             this.meanRRButton.Name = "meanRRButton";
             this.meanRRButton.Size = new System.Drawing.Size(75, 23);
             this.meanRRButton.TabIndex = 3;
@@ -127,7 +123,7 @@
             // qtcButton
             // 
             this.qtcButton.AutoSize = true;
-            this.qtcButton.Location = new System.Drawing.Point(332, 3);
+            this.qtcButton.Location = new System.Drawing.Point(161, 3);
             this.qtcButton.Name = "qtcButton";
             this.qtcButton.Size = new System.Drawing.Size(75, 23);
             this.qtcButton.TabIndex = 4;
@@ -136,13 +132,42 @@
             // 
             // imageButton
             // 
-            this.imageButton.Location = new System.Drawing.Point(413, 3);
+            this.imageButton.Location = new System.Drawing.Point(80, 3);
             this.imageButton.Name = "imageButton";
             this.imageButton.Size = new System.Drawing.Size(75, 23);
             this.imageButton.TabIndex = 5;
             this.imageButton.Text = "Image";
             this.imageButton.UseVisualStyleBackColor = true;
             this.imageButton.Click += new System.EventHandler(this.imageButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.panel1, 2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(682, 427);
+            this.panel1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(679, 424);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(3, 436);
+            this.trackBar1.Maximum = 5;
+            this.trackBar1.Minimum = -5;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 43);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // openFileDialog1
             // 
@@ -157,9 +182,12 @@
             this.Name = "Form1";
             this.Text = "EP Calipers";
             this.tableLayoutPanel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel1.PerformLayout();
             this.flowLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -167,7 +195,6 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button calibrateButton;
         private System.Windows.Forms.Button intervalRateButton;
@@ -176,6 +203,9 @@
         private System.Windows.Forms.Button imageButton;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button addCaliper;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
 
--- epcalipers/epcalipers/Form1.cs
+++ epcalipers/epcalipers/Form1.cs
@@ -13,11 +13,15 @@ namespace epcalipers
 {
     public partial class Form1 : Form
     {
+
+        Bitmap theBitmap;
+
         public Form1()
         {
             InitializeComponent();
-            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
-
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
         }
 
         private void imageButton_Click(object sender, EventArgs e)
@@ -26,6 +30,7 @@ namespace epcalipers
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 pictureBox1.Load(openFileDialog1.FileName);
+                theBitmap = new Bitmap(pictureBox1.Image);
             }
         }
 
@@ -57,6 +62,48 @@ namespace epcalipers
             Point mouseDownLocation = new Point(e.X, e.Y);
             Debug.WriteLine("mouse down {0}, {1}", e.X, e.Y);
 
+        }
+
+        private Bitmap zoom(Bitmap originalBitmap, Double zoomFactor)
+        {
+            Size newSize = new Size((int)(originalBitmap.Width * zoomFactor), (int)(originalBitmap.Height * zoomFactor));
+            Bitmap bmp = new Bitmap(originalBitmap, newSize);
+            return bmp;
+        }
+
+        private void pictureBox1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            //System.Diagnostics.Debug.WriteLine("paint");
+            //System.Diagnostics.Debug.WriteLine("PictureBox Top {0}, Bottom {1}", pictureBox1.Top, pictureBox1.Bottom);
+            //System.Diagnostics.Debug.WriteLine("Panel Top {0}, Bottom {1}", panel2.Top, panel2.Bottom);
+
+            // Create a local version of the graphics object for the PictureBox.
+            Graphics g = e.Graphics;
+            // Simulate a caliper
+            // Draw a string on the PictureBox.
+            String label = "200 points";
+            Font font = new Font("Arial", 10);
+            SizeF sizeOfString = g.MeasureString(label, font);
+            float width = sizeOfString.Width / 2.0f;
+            g.DrawString(label,
+                font, System.Drawing.Brushes.Blue, new Point(300 - (int)width, 180));
+            // Draw a line in the PictureBox.
+            g.DrawLine(System.Drawing.Pens.Blue, 200, 0,
+                200, pictureBox1.Height);
+            g.DrawLine(System.Drawing.Pens.Blue, 400, 0,
+                400, pictureBox1.Height);
+            g.DrawLine(System.Drawing.Pens.Blue, 200, 200,
+                400, 200);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (theBitmap == null)
+            {
+                return;
+            }
+            theBitmap = zoom(theBitmap, trackBar1.Value);
+            pictureBox1.Image = theBitmap;
         }
     }
 }
