80d4a8af0011454aa59b660d06e74efd4367e06d
mannd <mannd@DESKTOP-KS9168F>
2016-05-12 23:08:02 -0600

    Add settings

--- epcalipers/epcalipers/Properties/Settings.Designer.cs
+++ epcalipers/epcalipers/Properties/Settings.Designer.cs
@@ -8,22 +8,90 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace epcalipers.Properties
-{
-
-
+namespace epcalipers.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Blue")]
+        public global::System.Drawing.Color CaliperColorSetting {
+            get {
+                return ((global::System.Drawing.Color)(this["CaliperColorSetting"]));
+            }
+            set {
+                this["CaliperColorSetting"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Red")]
+        public global::System.Drawing.Color HighlightColorSetting {
+            get {
+                return ((global::System.Drawing.Color)(this["HighlightColorSetting"]));
+            }
+            set {
+                this["HighlightColorSetting"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("2")]
+        public int LineWidthSetting {
+            get {
+                return ((int)(this["LineWidthSetting"]));
+            }
+            set {
+                this["LineWidthSetting"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1000 msec")]
+        public string TimeCalibrationSetting {
+            get {
+                return ((string)(this["TimeCalibrationSetting"]));
+            }
+            set {
+                this["TimeCalibrationSetting"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1 mV")]
+        public string AmplitudeCalibrationSetting {
+            get {
+                return ((string)(this["AmplitudeCalibrationSetting"]));
+            }
+            set {
+                this["AmplitudeCalibrationSetting"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool RoundMsecRateSetting {
+            get {
+                return ((bool)(this["RoundMsecRateSetting"]));
+            }
+            set {
+                this["RoundMsecRateSetting"] = value;
             }
         }
     }
