d2df87033b661e95b4696f1cf3bb386c9d02af52
mannd <mannd@DESKTOP-KS9168F>
2016-10-02 19:32:13 -0600

    Add convert_file.ps1 script
    Also added appx-manifest-assets folder with assets that need to be
    copied to generated appx folder.

---
+++ scripts/convert_app.ps1
@@ -0,0 +1,1 @@
+DesktopAppConverter.exe -Installer C:\Users\mannd\git\epcalipers-windows\epcalipers\epcalipers-install\bin\x64\Release\epcalipers_install.msi  -Destination C:\Users\mannd\Documents\epcalipers-app\ -PackageName "epcalipers" -Publisher "CN=EP Studios" -Version 1.1.1.0 -MakeAppx -Verbose
