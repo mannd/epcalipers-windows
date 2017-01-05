cef0abd818f2d8122267675972d5f87ef2210c98
mannd <mannd@DESKTOP-KS9168F>
2017-01-05 08:40:07 -0700

    Update to .NET 4.6.1

--- epcalipers/epcalipersTests/CalibrationTests.cs
+++ epcalipers/epcalipersTests/CalibrationTests.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using epcalipers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace epcalipers.Tests
 {
