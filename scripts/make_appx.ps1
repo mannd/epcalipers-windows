ed45d1d0cf8269c688e7c8453f59b44e7bd3cc85
mannd <mannd@DESKTOP-KS9168F>
2017-01-06 12:03:21 -0700

    Make changes for App Store submission

--- scripts/make_appx.ps1
+++ scripts/make_appx.ps1
@@ -1,4 +1,4 @@
 cd "C:\Program Files (x86)\Windows Kits\10\bin\x64"
 .\makeappx.exe pack /d C:\Users\mannd\Documents\epcalipers-app\epcalipers\PackageFiles\ /p C:\Users\mannd\Documents\epcalipers-app\epcalipers\epcalipers
-.\signtool.exe sign -f my.pfx -fd SHA256 -v C:\Users\mannd\Documents\epcalipers-app\epcalipers\epcalipers.appx
+.\signtool.exe sign -f epstudios.pfx -fd SHA256 -v C:\Users\mannd\Documents\epcalipers-app\epcalipers\epcalipers.appx
 cd "C:\Users\mannd\git\epcalipers-windows\scripts"
