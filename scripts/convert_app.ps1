0a326842e7b415f6b0ffb2ea5ab0be1aa01400ce
mannd <mannd@DESKTOP-KS9168F>
2017-05-06 15:47:38 -0600

    Bump version to 1.3

--- scripts/convert_app.ps1
+++ scripts/convert_app.ps1
@@ -1,1 +1,1 @@
-DesktopAppConverter.exe -Installer C:\Users\mannd\git\epcalipers-windows\epcalipers\epcalipers-install\bin\x64\Release\epcalipers_install.msi  -Destination C:\Users\mannd\Documents\epcalipers-app\ -PackageName "epcalipers" -Publisher "CN=EP Studios" -Version 1.2.0.0 -MakeAppx -Verbose
+DesktopAppConverter.exe -Installer C:\Users\mannd\git\epcalipers-windows\epcalipers\epcalipers-install\bin\x64\Release\epcalipers_install.msi  -Destination C:\Users\mannd\Documents\epcalipers-app\ -PackageName "epcalipers" -Publisher "CN=EP Studios" -Version 1.3.0.0 -MakeAppx -Verbose
