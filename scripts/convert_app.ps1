005479e87b93603999326d4b4fe083022b314b61
mannd <mannd@DESKTOP-KS9168F>
2018-10-21 00:04:09 -0600

    Add separate install projects for x86 and x64

--- scripts/convert_app.ps1
+++ scripts/convert_app.ps1
@@ -2,5 +2,10 @@ param (
     [string]$platform = "x86",
     [string]$version = "1.10.0.0"
 )
+[string]$installer = "C:\Users\mannd\git\epcalipers-windows\epcalipers\epcalipers-install\bin\$platform\Release\epcalipers_install.msi"
+If ($platform -eq "x64")
+{
+    $installer = "C:\Users\mannd\git\epcalipers-windows\epcalipers\epcalipers-x64-install\bin\$platform\Release\epcalipers_install.msi"
+}
 
-DesktopAppConverter.exe -Installer C:\Users\mannd\git\epcalipers-windows\epcalipers\epcalipers-install\bin\$platform\Release\epcalipers_install.msi  -Destination C:\Users\mannd\Documents\epcalipers-app\ -PackageName "epcalipers" -Publisher "CN=EP Studios" -Version $version -MakeAppx -Verbose
+DesktopAppConverter.exe -Installer $installer -Destination C:\Users\mannd\Documents\epcalipers-app\ -PackageName "epcalipers" -Publisher "CN=EP Studios" -Version $version -MakeAppx -Verbose
