db9d3abf439479122b5b87240025873728661703
mannd <mannd@DESKTOP-KS9168F>
2018-10-26 12:24:35 -0600

    Bump version to 2.0.0

--- epcalipers/SharedAssemblyInfo.cs
+++ epcalipers/SharedAssemblyInfo.cs
@@ -26,4 +26,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0.0-alpha")] // a.k.a. "Product version"	
+[assembly: AssemblyInformationalVersion("2.0.0")] // a.k.a. "Product version"	
