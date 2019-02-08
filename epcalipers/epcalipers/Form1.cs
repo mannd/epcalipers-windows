06099e044de1aadb4b843a312020e9521b118895
mannd <mannd@DESKTOP-KS9168F>
2019-02-08 09:16:35 -0700

    Update buttons in transparent window

--- epcalipers/epcalipers/Form1.cs
+++ epcalipers/epcalipers/Form1.cs
@@ -142,10 +142,10 @@ namespace epcalipers
             InitButton(calibrateButton, "Calibration", "Calibrate, recalibrate or clear calibration", calibrateButton_Click);
 
             setCalibrationButton = new Button();
-            InitButton(setCalibrationButton, "Set calibration", "Set calibration interval", setCalibrationButton_Click);
+            InitButton(setCalibrationButton, "Set Calibration", "Set calibration interval", setCalibrationButton_Click);
 
             clearCalibrationButton = new Button();
-            InitButton(clearCalibrationButton, "Clear calibration", "Clear all calibration", clearCalibrationButton_Click);
+            InitButton(clearCalibrationButton, "Clear Calibration", "Clear all calibration", clearCalibrationButton_Click);
 
             backCalibrationButton = new Button();
             InitButton(backCalibrationButton, "Done", "Done with calibration", backCalibrationButton_Click);
