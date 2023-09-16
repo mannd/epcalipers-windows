a7831c48427d05fd4d08d4dc062463fd1f127ced
manndmd <manndmd@gmail.com>
2023-09-15 18:07:27 -0600

    Add About view model

---
+++ epcalipers/EPCalipersWinUI3/ViewModels/AboutViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+using EPCalipersWinUI3.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPCalipersWinUI3.ViewModels
+{
+	public partial class AboutViewModel : ObservableObject
+	{
+
+		[ObservableProperty]
+		private string title = $"About {AssemblyProperties.AssemblyProduct}";
+
+		[ObservableProperty]
+		private string appName = AssemblyProperties.AssemblyProduct;
+
+		[ObservableProperty]
+		private string version = String.Format(CultureInfo.CurrentCulture,
+				"Version {0}", AssemblyProperties.AssemblyVersion);
+
+		[ObservableProperty]
+		private string copyright = $"{AssemblyProperties.AssemblyCopyright} {AssemblyProperties.AssemblyCompany}";
+
+		[ObservableProperty]
+		private string homePage = "EP Studios home page";
+
+		[ObservableProperty]
+		private string navigateUri = "https://www.epstudiossoftware.com";
+	}
+}
--- epcalipers/EPCalipersWinUI3/Views/AboutDialog.xaml.cs
+++ epcalipers/EPCalipersWinUI3/Views/AboutDialog.xaml.cs
@@ -16,69 +16,18 @@ using Windows.Foundation.Collections;
 using EPCalipersWinUI3.Models;
 using Microsoft.UI.Xaml.Documents;
 using System.Diagnostics;
+using EPCalipersWinUI3.ViewModels;
 
 namespace EPCalipersWinUI3.Views
 {
 	public sealed partial class AboutDialog : ContentDialog
 	{
+		public AboutViewModel ViewModel { get; set; }
 
 		public AboutDialog()
 		{
+			ViewModel = new AboutViewModel();
 			this.InitializeComponent();
-			AdditionalInitiation();
-		}
-
-		private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
-		{
-		}
-
-		private void AdditionalInitiation()
-		{
-			Title = $"About {AssemblyProperties.AssemblyProduct}";
-			AppName.Text = AssemblyProperties.AssemblyProduct;
-			SetVersion();
-			Copyright.Text = $"{AssemblyProperties.AssemblyCopyright} {AssemblyProperties.AssemblyCompany}";
-			Debug.Write(Copyright.Text);
-			//SetConfiguration();
-			//DescriptionText.Text = AssemblyProperties.AssemblyDescription;
-		}
-
-		//private void SetCompany()
-		//{
-		//	Company.Text = AssemblyProperties.AssemblyCompany;
-		//	//Run run = new(AssemblyProperties.AssemblyCompany);
-		//	//Hyperlink hyperlink = new(run)
-		//	//{
-		//	//	NavigateUri = new Uri("https://www.epstudiossoftware.com")
-		//	//};
-		//	//hyperlink.RequestNavigate += Hyperlink_RequestNavigate;
-		//	//Company.Text = null;
-		//	//Company.Inlines.Add(hyperlink);
-		//}
-
-		//private void SetConfiguration()
-		//{
-		//	string? configuration = AssemblyProperties.AssemblyConfigurationAttribute;
-		//	if (configuration == null || configuration == "")
-		//	{
-		//		Configuration.Text = "Generic configuration";
-		//	}
-		//	else
-		//	{
-		//		Configuration.Text = configuration;
-		//	}
-		//}
-
-		private void SetVersion(bool detailed = false)
-		{
-			if (detailed)
-			{
-				this.Version.Text = String.Format(CultureInfo.CurrentCulture,
-					"Version {0} ({1})", AssemblyProperties.AssemblyVersion, AssemblyProperties.AssemblyFileVersion);
-				return;
-			}
-			this.Version.Text = String.Format(CultureInfo.CurrentCulture,
-				"Version {0}", AssemblyProperties.AssemblyVersion);
 		}
 	}
 }
