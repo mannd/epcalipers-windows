2ac9fa5fd2e570ea0dce1a1a670939b0d5f0c9cf
manndmd <manndmd@gmail.com>
2023-10-26 20:33:38 -0600

    Proof of concept transparent WinUI3 window

--- epcalipers/EPCalipersWinUI3/MainWindow.xaml.cs
+++ epcalipers/EPCalipersWinUI3/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace EPCalipersWinUI3
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public sealed partial class MainWindow : WinUIEx.WindowEx
     {
 
         public MainWindow()
