ee67837ea38eca002ea4cebb441a54fab842de32
manndmd <manndmd@gmail.com>
2023-11-21 10:24:51 -0700

    Improve layout of settings page

--- epcalipers/EPCalipersWinUi3Tests/Tests/CaliperLabelTests.cs
+++ epcalipers/EPCalipersWinUi3Tests/Tests/CaliperLabelTests.cs
@@ -56,34 +56,36 @@ namespace EPCalipersWinUi3Tests.Tests
 			var stubCaliperView = new FakeCaliperView(); // Bounds(800, 400)
 			var stubSettings = new FakeSettings();
 			var caliper = new AmplitudeCaliper(position, stubCaliperView, stubSettings, true);
-			var alignment = CaliperLabelAlignment.Top;
+			var alignment = CaliperLabelAlignment.Left;
 			var label = new AmplitudeCaliperLabel(caliper, stubCaliperView, "100 points",
 				alignment, false, true);
 			label.Size = new Windows.Foundation.Size(100, 50);
 			var autoAlignment = label.AutoAlign(alignment, false);
-			Assert.Equal(CaliperLabelAlignment.Top, autoAlignment);
-			caliper.CrossBar.Position = 100;
+			Assert.Equal(CaliperLabelAlignment.Left, autoAlignment);
+			caliper.CrossBar.Position = 200;
 			caliper.TopBar.Position = 100;
 			caliper.BottomBar.Position = 300;  // Give enough room to fit the label in between
 			autoAlignment = label.AutoAlign(alignment, true);
-			Assert.Equal(CaliperLabelAlignment.Top, autoAlignment);
+			Assert.Equal(CaliperLabelAlignment.Left, autoAlignment);
 			caliper.TopBar.Position = 200;
 			caliper.BottomBar.Position = 210;  // make them too close
 			autoAlignment = label.AutoAlign(alignment, true);
-			Assert.Equal(CaliperLabelAlignment.Left, autoAlignment);
-			caliper.TopBar.Position = 50;  // too close to left edge of view
-			caliper.BottomBar.Position = 100;  // make them too close
+			Assert.Equal(CaliperLabelAlignment.Top, autoAlignment);
+			caliper.TopBar.Position = 50;  
+			caliper.BottomBar.Position = 200;
+			caliper.CrossBar.Position = 50;  // too close to left edge of view
 			autoAlignment = label.AutoAlign(alignment, true);
 			Assert.Equal(CaliperLabelAlignment.Right, autoAlignment);
 			alignment = CaliperLabelAlignment.Right;
 			caliper.TopBar.Position = 500;
 			caliper.BottomBar.Position = 600;  // plenty of room all around
-			//autoAlignment = label.AutoAlign(alignment, true);
-			//Assert.Equal(CaliperLabelAlignment.Right, autoAlignment);
-			//caliper.TopBar.Position = 500;
-			//caliper.BottomBar.Position = 710;  // too close to right side of view
-			//autoAlignment = label.AutoAlign(alignment, true);
-			//Assert.Equal(CaliperLabelAlignment.Left, autoAlignment);
+			autoAlignment = label.AutoAlign(alignment, true);
+			Assert.Equal(CaliperLabelAlignment.Right, autoAlignment);
+			caliper.TopBar.Position = 200;
+			caliper.BottomBar.Position = 300;
+			caliper.CrossBar.Position = 780; // Too close to right margin
+			autoAlignment = label.AutoAlign(alignment, true);
+			Assert.Equal(CaliperLabelAlignment.Left, autoAlignment);
 		}
 	}
 }
