1aa0e5e8e8bbaba78f5447708389f14fd7ec98a7
manndmd <manndmd@gmail.com>
2024-04-13 22:41:42 +0200

    WinUI3PDFHandler meets StyleCop rules

--- epcalipers/WinUI3PDFHandler/IPdfHelper.cs
+++ epcalipers/WinUI3PDFHandler/IPdfHelper.cs
@@ -1,22 +1,87 @@
-﻿using Microsoft.UI.Xaml.Media.Imaging;
+﻿//-----------------------------------------------------------------------
+// <copyright file="IPdfHelper.cs" company="EP Studios">
+// Copyright (c) EP Studios. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
 using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Media.Imaging;
 using Windows.Storage;
 
 namespace EPCalipersWinUI3PDFHandler
 {
-	public interface IPdfHelper
+    /// <summary>
+    /// Interface for classes providing PDF functions.
+    /// </summary>
+    public interface IPdfHelper
     {
+        /// <summary>
+        /// Gets current PDF page number.
+        /// Page numbering is 1 based.
+        /// </summary>
         int CurrentPageNumber { get; }
+
+        /// <summary>
+        /// Gets or sets path to PDF file.
+        /// </summary>
         string FilePath { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating whether PDF has multiple pages.
+        /// </summary>
         bool IsMultiPage { get; }
+
+        /// <summary>
+        /// Gets the highest page number of a PDF document.
+        /// This is 1 based.
+        /// </summary>
         int MaximumPageNumber { get; }
+
+        /// <summary>
+        /// Gets the number of pages of a PDF.
+        /// </summary>
         int NumberOfPdfPages { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether a PDF file is loaded.
+        /// </summary>
         bool PdfIsLoaded { get; }
 
+        /// <summary>
+        /// Clears and nullifies the PDF document.
+        /// </summary>
         void ClearPdfFile();
+
+        /// <summary>
+        /// Gets bitmap of next PDF page.
+        /// </summary>
+        /// <returns>Bitmap of next PDF page or null if no next page.</returns>
         Task<SoftwareBitmapSource> GetNextPage();
+
+        /// <summary>
+        /// Get a PDF page asynchronously.
+        /// </summary>
+        /// <param name="pageNumber">Page number, 1 based.</param>
+        /// <returns>Page Bitmap.</returns>
         Task<SoftwareBitmapSource> GetPdfPageSourceAsync(int pageNumber);
+
+        /// <summary>
+        /// Gets bitmap of previous PDF page.
+        /// </summary>
+        /// <returns>Bitmap of previous PDF page or null if no previous page.</returns>
         Task<SoftwareBitmapSource> GetPreviousPage();
+
+        /// <summary>
+        /// Checks whether a file contains a PDF document.
+        /// </summary>
+        /// <param name="file">File to be checked.</param>
+        /// <returns>True is file is a PDF document file.</returns>
+        bool IsPdfFile(StorageFile file);
+
+        /// <summary>
+        /// Load a PDF document from a file.
+        /// </summary>
+        /// <param name="file">File containing the PDF document.</param>
         void LoadPdfFile(StorageFile file);
     }
 }
--- epcalipers/WinUI3PDFHandler/PdfHelper.cs
+++ epcalipers/WinUI3PDFHandler/PdfHelper.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.UI.Xaml.Media.Imaging;
-using PdfiumViewer;
+﻿//-----------------------------------------------------------------------
+// <copyright file="PdfHelper.cs" company="EP Studios">
+// Copyright (c) EP Studios. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
 using System;
 using System.Diagnostics;
 using System.Drawing;
@@ -7,118 +11,162 @@ using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using Windows.Storage;
+using Microsoft.UI.Xaml.Media.Imaging;
+using PdfiumViewer;
 using Windows.Graphics.Imaging;
+using Windows.Storage;
 
 namespace EPCalipersWinUI3PDFHandler
 {
+    /// <summary>
+    /// Encapsulate and isolate nasty PDF code.
+    /// This uses the Pdfium NuGet package.
+    /// </summary>
+    public class PdfHelper : IPdfHelper
+    {
+        private PdfDocument _pdfDocument = null;
+        private int _pageNumber = 0;
 
-	/// <summary>
-	/// Encapsulate and isolate nasty PDF code.
-	/// </summary>
-	public class PdfHelper : IPdfHelper
-	{
-		private PdfDocument _pdfDocument = null;
-		private int _pageNumber = 0;
+        /// <inheritdoc/>
+        public string FilePath { get; set; }
 
-		public string FilePath { get; set; }
-		public bool PdfIsLoaded => _pdfDocument != null;
+        /// <inheritdoc/>
+        public bool PdfIsLoaded => _pdfDocument != null;
 
-		public int NumberOfPdfPages => _pdfDocument.PageCount;
+        /// <inheritdoc/>
+        public int NumberOfPdfPages => _pdfDocument.PageCount;
 
-		// Two properties below use 1 based pagination.
-		public int CurrentPageNumber => _pageNumber + 1;
+        /// <inheritdoc/>
+        public int CurrentPageNumber => _pageNumber + 1;
 
-		public int MaximumPageNumber => NumberOfPdfPages;
+        /// <inheritdoc/>
+        public int MaximumPageNumber => NumberOfPdfPages;
 
-		public static bool IsPdfFile(StorageFile file) =>
-			file.FileType.ToUpper() == ".PDF";
+        /// <inheritdoc/>
+        public bool IsMultiPage
+        {
+            get
+            {
+                return _pdfDocument?.PageCount > 0;
+            }
+        }
 
-		public void LoadPdfFile(StorageFile file)
-		{
-			if (file == null)
-			{
-				return;
-			}
-			try
-			{
-				_pdfDocument = PdfDocument.Load(file.Path);
-				_pageNumber = 0;
-			}
-			catch (Exception ex)
-			{
-				Debug.WriteLine(ex.Message);
-			}
-		}
+        /// <inheritdoc/>
+        public bool IsPdfFile(StorageFile file) =>
+            file.FileType.Equals(".PDF", StringComparison.CurrentCultureIgnoreCase);
 
-		public async Task<SoftwareBitmapSource> GetPdfPageSourceAsync(int pageNumber)
-		{
-			if (_pdfDocument == null) { return null; }
-			if (pageNumber < 0 || pageNumber > _pdfDocument.PageCount - 1) { return null; }
-			_pageNumber = pageNumber;
-			var img = _pdfDocument.Render(pageNumber, 300, 300, PdfRenderFlags.CorrectFromDpi);
-			var source = await GetWinUI3BitmapSourceFromGdiBitmap(new Bitmap(img));
-			img.Dispose();
-			return source;
-		}
+        /// <inheritdoc/>
+        public void LoadPdfFile(StorageFile file)
+        {
+            if (file == null)
+            {
+                return;
+            }
 
-		public void ClearPdfFile()
-		{
-			_pdfDocument?.Dispose();
-			_pdfDocument = null;
-			_pageNumber = 0;
-		}
+            try
+            {
+                _pdfDocument = PdfDocument.Load(file.Path);
+                _pageNumber = 0;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
 
-		public bool IsMultiPage
-		{
-			get
-			{
-				return _pdfDocument?.PageCount > 0;
-			}
-		}
+        /// <inheritdoc/>
+        public async Task<SoftwareBitmapSource> GetPdfPageSourceAsync(int pageNumber)
+        {
+            if (_pdfDocument == null)
+            {
+                return null;
+            }
 
-		public async Task<SoftwareBitmapSource> GetNextPage()
-		{
-			if (_pdfDocument == null) return null;
-			int nextPage = _pageNumber + 1;
-			if (nextPage > _pdfDocument.PageCount - 1) return null;
-			_pageNumber = nextPage;
-			return await GetPdfPageSourceAsync(nextPage);
-		}
+            if (pageNumber < 0 || pageNumber > _pdfDocument.PageCount - 1)
+            {
+                return null;
+            }
 
-		public async Task<SoftwareBitmapSource> GetPreviousPage()
-		{
-			if (_pdfDocument == null) return null;
-			int previousPage = _pageNumber - 1;
-			if (previousPage < 0) return null;
-			_pageNumber = previousPage;
-			return await GetPdfPageSourceAsync(previousPage);
-		}
+            _pageNumber = pageNumber;
+            var img = _pdfDocument.Render(pageNumber, 300, 300, PdfRenderFlags.CorrectFromDpi);
+            var source = await GetWinUI3BitmapSourceFromGdiBitmap(new Bitmap(img));
+            img.Dispose();
+            return source;
+        }
 
-		// From https://stackoverflow.com/questions/76640972/convert-system-drawing-icon-to-microsoft-ui-xaml-imagesource
-		private static async Task<SoftwareBitmapSource> GetWinUI3BitmapSourceFromGdiBitmap(Bitmap bmp)
-		{
-			if (bmp == null)
-				return null;
-			// get pixels as an array of bytes
-			var data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
-				ImageLockMode.ReadOnly, bmp.PixelFormat);
-			var bytes = new byte[data.Stride * data.Height];
-			Marshal.Copy(data.Scan0, bytes, 0, bytes.Length);
-			bmp.UnlockBits(data);
+        /// <inheritdoc/>
+        public void ClearPdfFile()
+        {
+            _pdfDocument?.Dispose();
+            _pdfDocument = null;
+            _pageNumber = 0;
+        }
 
-			// get WinRT SoftwareBitmap
-			var softwareBitmap = new SoftwareBitmap(
-				BitmapPixelFormat.Bgra8,
-				bmp.Width,
-				bmp.Height,
-				BitmapAlphaMode.Premultiplied);
-			softwareBitmap.CopyFromBuffer(bytes.AsBuffer());
+        /// <inheritdoc/>
+        public async Task<SoftwareBitmapSource> GetNextPage()
+        {
+            if (_pdfDocument == null)
+            {
+                return null;
+            }
 
-			// build WinUI3 SoftwareBitmapSource
-			var source = new SoftwareBitmapSource();
-			await source.SetBitmapAsync(softwareBitmap);
-			return source;
-		}
-	}
+            int nextPage = _pageNumber + 1;
+            if (nextPage > _pdfDocument.PageCount - 1)
+            {
+                return null;
+            }
+
+            _pageNumber = nextPage;
+            return await GetPdfPageSourceAsync(nextPage);
+        }
+
+        /// <inheritdoc/>
+        public async Task<SoftwareBitmapSource> GetPreviousPage()
+        {
+            if (_pdfDocument == null)
+            {
+                return null;
+            }
+
+            int previousPage = _pageNumber - 1;
+            if (previousPage < 0)
+            {
+                return null;
+            }
+
+            _pageNumber = previousPage;
+            return await GetPdfPageSourceAsync(previousPage);
+        }
+
+        // From https://stackoverflow.com/questions/76640972/convert-system-drawing-icon-to-microsoft-ui-xaml-imagesource
+        private static async Task<SoftwareBitmapSource> GetWinUI3BitmapSourceFromGdiBitmap(Bitmap bmp)
+        {
+            if (bmp == null)
+            {
+                return null;
+            }
+
+            // get pixels as an array of bytes
+            var data = bmp.LockBits(
+                new Rectangle(0, 0, bmp.Width, bmp.Height),
+                ImageLockMode.ReadOnly,
+                bmp.PixelFormat);
+            var bytes = new byte[data.Stride * data.Height];
+            Marshal.Copy(data.Scan0, bytes, 0, bytes.Length);
+            bmp.UnlockBits(data);
+
+            // get WinRT SoftwareBitmap
+            var softwareBitmap = new SoftwareBitmap(
+                BitmapPixelFormat.Bgra8,
+                bmp.Width,
+                bmp.Height,
+                BitmapAlphaMode.Premultiplied);
+            softwareBitmap.CopyFromBuffer(bytes.AsBuffer());
+
+            // build WinUI3 SoftwareBitmapSource
+            var source = new SoftwareBitmapSource();
+            await source.SetBitmapAsync(softwareBitmap);
+            return source;
+        }
+    }
 }
