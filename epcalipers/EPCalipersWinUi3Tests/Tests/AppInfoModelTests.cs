f6a46c9b05f0a225aa6e9695edc53cff24978a86
manndmd <manndmd@gmail.com>
2025-03-07 20:49:40 -0700

    Remove lingering references to Pdfium NuGet package

--- epcalipers/EPCalipersWinUi3Tests/Tests/AppInfoModelTests.cs
+++ epcalipers/EPCalipersWinUi3Tests/Tests/AppInfoModelTests.cs
@@ -20,7 +20,7 @@ namespace EPCalipersWinUi3Tests.Tests
 			//Assert.Equal("3.0.0.0", appInfo.FileVersion);
 			Assert.Equal("EPCalipersWinUI3", appInfo.Title);
 			Assert.Equal("EP Calipers 3", appInfo.ProductName);
-			Assert.Equal("Copyright (c) 2023-2024", appInfo.Copyright);
+			Assert.Equal("Copyright (c) 2023-2025", appInfo.Copyright);
 			Assert.Equal("EP Studios", appInfo.Company);
 		}
 	}
