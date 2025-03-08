dc4c97b617449fd1d54c9f8a70cc9a4118818d20
manndmd <manndmd@gmail.com>
2025-03-08 10:25:32 -0700

    Delete old scripts

--- scripts/create_x86_package.ps1
+++
@@ -1,9 +0,0 @@
-param (
-    [Parameter(Mandatory=$true)][string]$version = ""
-)
-if ([string]::isNullOrEmpty($version))
-{
-    Write-Warning "Must supply -version parameter on command line."
-    Break
-}
-.\create_package -platform x86 -version $version
